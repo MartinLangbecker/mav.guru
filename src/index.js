8803a20a7d2db25ba7084aff733b1d176a0c0534
Martin Langbecker <Martin.Langbecker@gmail.com>
2023-01-08 02:13:54 +0100

    remove bus API

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import createServer from './lib/index.js'
-import * as bahn from './apis/bahn/index.js'
-import * as bus from './apis/bus/index.js'
+import createServer from './lib/index.js';
+import * as bahn from './apis/bahn/index.js';
 
-const port = (+process.env.PORT) || 3000
-const apiId = process.env.API || 'bahn'
+const port = +process.env.PORT || 3000;
+const apiId = process.env.API || 'bahn';
 
-const apis = { bahn, bus }
-const api = apis[apiId]
-if (!api) throw new Error('Unknown api selected.')
+const apis = { bahn };
+const api = apis[apiId];
+if (!api) throw new Error('Unknown api selected.');
 
-const server = createServer(api)
+const server = createServer(api);
 
 // start HTTP server
-server.listen(port, error => {
-	if (error) return console.error(error)
-	console.log(`HTTP: Listening on port ${port}.`)
-})
+server.listen(port, (error) => {
+  if (error) return console.error(error);
+  console.log(`HTTP: Listening on port ${port}.`);
+});
